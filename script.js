053a68f4cd6b60141f1f70211078fa39662c99f7
Glenn Aaroen <glaar90@gmail.com>
2022-11-24 21:57:20 +0100

    Add more boundaries

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var Engine = Matter.Engine,
 
 var engine;
 var circles = [];
-var boundarys = [];
+var boundaries = [];
 var world;
 
 var ground;
@@ -18,8 +18,9 @@ function setup() {
     world = engine.world;
     // Engine.run(engine);
 
-    boundarys.push(new Boundary(300, 200, width*0.6, 25, 0.3))
-    boundarys.push(new Boundary(600, 100, width*0.6, 25, -0.3))
+    boundaries.push(new Boundary(600, 50, width*0.6, 25, -0.3))
+    boundaries.push(new Boundary(200, 150, width*0.6, 25, 0.3))
+    boundaries.push(new Boundary(600, 250, width*0.6, 25, -0.3))
 }
 
 function mouseDragged() {
@@ -35,8 +36,8 @@ function draw() {
         circles[i].show();
     }
 
-    for (var i = 0; i < boundarys.length; i++) {
-        boundarys[i].show();
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].show();
     }
 
 }
